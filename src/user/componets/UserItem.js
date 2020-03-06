aeab5ae1c74244594eb32b83c2d419b5ce972dfe
Serguei Rodriguez <sjosevich@gmail.com>
2020-03-05 19:43:43 -0700

    Starting our project

---
+++ src/user/componets/UserItem.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './UserItem.css'
+
+const UserItem = props => {};
+
+export default UserItem;
---
+++ src/user/pages/Users.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Users = () => {
+    return <h2>Users Work!!!</h2>
+}
+
+export default Users;
+
