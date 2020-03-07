413fef0b661a87699e22d07e463f014eae214d26
ichumats <ivanna.chumatschko22@gmail.com>
2020-03-07 11:25:55 -0700

    adding routes and shelterController

---
+++ controllers/sheltersController.js
@@ -0,0 +1,37 @@
+const db = require("../models");
+
+// Defining methods for the sheltersController
+module.exports = {
+  findAll: function(req, res) {
+    db.Shelter
+      .find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    db.Shelter
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    db.Shelter
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  update: function(req, res) {
+    db.Shelter
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    db.Shelter
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+};
