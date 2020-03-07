486043dc15273ad85ad81fae78f283001a7ba2da
ichumats <ivanna.chumatschko22@gmail.com>
2020-03-07 11:07:03 -0700

    updating server.js to get index.html from public folder

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV === "production") {
 // Send every request to the React app
 // Define any API routes before this runs
 app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/public/index.html"));
 });
 
 app.listen(PORT, function() {
