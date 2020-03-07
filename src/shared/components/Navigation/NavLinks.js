2176eab10ca9c73a4225354537457e11b464123c
Serguei Rodriguez <sjosevich@gmail.com>
2020-03-06 22:59:42 -0700

    Added a drawer component with some animation

--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import './NavLinks.css';
 
 const NavLinks = props => {
-   return <ul className="nav-links">
-      <li>
-        <NavLink to="/" exact>ALL Providers</NavLink>
-      </li>
-      <li>
-        <NavLink to="/u1/places">PLACES</NavLink>
-      </li> 
-      <li>
-        <NavLink to="/places/new">ADD PLACE</NavLink>
-      </li>
-      <li>
-        <NavLink to="/auth">AUTHENTICATE</NavLink>
-      </li>
-   </ul>
+  return <ul className="nav-links">
+    <li>
+      <NavLink to="/" exact>ALL USERS</NavLink>
+    </li>
+    <li>
+      <NavLink to="/u1/places">MY PLACES</NavLink>
+    </li>
+    <li>
+      <NavLink to="/places/new">ADD PLACE</NavLink>
+    </li>
+    <li>
+      <NavLink to="/auth">AUTHENTICATE</NavLink>
+    </li>
+  </ul>
 };
 
 export default NavLinks;
-
