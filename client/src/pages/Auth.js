98217ac46992878f8972fca0f696937f4ad9a678
Serguei Rodriguez <sjosevich@gmail.com>
2020-03-10 16:32:03 -0700

    Created the Authenticate form

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -5,27 +5,65 @@ import Input from '../components/Formelements/Input';
 import Button from '../components/Formelements/Button';
 import {
     VALIDATOR_EMAIL,
-    VALIDTOR_MINLENGTH
+    VALIDATOR_MINLENGTH
 } from '../utils/validators';
+import {useForm} from '../components/Form/form-hook';
 import './Auth.css';
 
 const Auth = () => {
-  return <Card className="authentication">
-    <h2>Login Required </h2>
-    <hr/>
-    <form>
-       <Input 
-       element="input" 
-       id="email" 
-       type="email" 
-       label="E-Mail" 
-       validators={[VALIDATOR_EMAIL()]} 
-       errorText="Please enter a valid email address."
-       />
-    </form>
-  </Card>
 
-};
-
-
-export default Auth;
+    const [formState, inputHandler] = useForm(
+      {
+        email: {
+          value: '',
+          isValid: false
+        },
+        password: {
+          value: '',
+          isValid: false
+        }
+      },
+      false
+    );
+    
+  
+    const authSubmitHandler = event => {
+      event.preventDefault();
+      console.log(formState.inputs);
+    };
+  
+    // const handleSubmitted = ({ res, fields, form }) => {
+    //     form.reset() // resets 
+    //   }
+    return (
+      <Card className="authentication">
+        <h2>Login Required</h2>
+        <hr />
+        <form onSubmit={authSubmitHandler}>
+          <Input
+            element="input"
+            id="email"
+            type="email"
+            label="E-Mail"
+            validators={[VALIDATOR_EMAIL()]}
+            errorText="Please enter a valid email address."
+            onInput={inputHandler}
+          />
+          <Input
+            element="input"
+            id="password"
+            type="password"
+            label="Password"
+            validators={[VALIDATOR_MINLENGTH(5)]}
+            errorText="Please enter a valid password, at least 5 characters."
+            onInput={inputHandler}
+          />
+          <Button type="submit" disabled={!formState.isValid}>
+            LOGIN
+          </Button>
+        </form>
+      </Card>
+    );
+  };
+  
+  export default Auth;
