3737662d136ff703bee4d8e40755d282126fea0d
Serguei Rodriguez <sjosevich@gmail.com>
2020-03-10 11:52:07 -0700

    Added the util validator in order to validate some inputs as the user email and password

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
+import Card from '../components/Card/Card';
+import Input from '../components/Formelements/Input';
+import Button from '../components/Formelements/Button';
 import './Auth.css';
 
 const Auth = () => {
-  return <h2>Auth!</h2>
+  return <Card className="authentication">
+    <h2>Login Required </h2>
+    <hr/>
+    <form>
+       
+    </form>
+  </Card>
 
 };
 
