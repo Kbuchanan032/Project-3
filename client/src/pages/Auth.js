a10bd2ab1d44a1e3bb23653123a3f4c2ab5f3c45
Serguei Rodriguez <sjosevich@gmail.com>
2020-03-10 12:03:44 -0700

    Added the form-hook in orde to validate the form

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -3,6 +3,10 @@ import React from 'react';
 import Card from '../components/Card/Card';
 import Input from '../components/Formelements/Input';
 import Button from '../components/Formelements/Button';
+import {
+    VALIDATOR_EMAIL,
+    VALIDTOR_MINLENGTH
+} from '../utils/validators';
 import './Auth.css';
 
 const Auth = () => {
@@ -10,7 +14,14 @@ const Auth = () => {
     <h2>Login Required </h2>
     <hr/>
     <form>
-       
+       <Input 
+       element="input" 
+       id="email" 
+       type="email" 
+       label="E-Mail" 
+       validators={[VALIDATOR_EMAIL()]} 
+       errorText="Please enter a valid email address."
+       />
     </form>
   </Card>
 
