c045f7ca8c4cac70b8216595da3dd47cd666f3d5
ichumats <ivanna.chumatschko22@gmail.com>
2020-03-12 22:04:09 -0700

    updating static assets

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/public"));
+  app.use(express.static("client/build"));
 }
 
 //Add routes, both API and view
